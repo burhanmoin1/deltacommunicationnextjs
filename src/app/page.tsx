c8547f01152de5d8f81d50e4aa6648912017f292
Burhan <m.burhanmoin1@gmail.com>
2024-05-04 09:17:09 +0500

    'icon'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Footer from './Footer';
 export default function Home() {
   return (
     <main>
+      <link rel="icon" href="./favicon.ico"/>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>      <DeltaNavbar />
       <Cablegif />
       <RocketContainer />
